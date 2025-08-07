9d61018d124ef4f5d6622ec409eec7ac72c24706
Azzam S.A <vcs@azzamsa.com>
2025-08-08 05:06:20 +0700

    fix: same speed in each runs

--- extension.js
+++ extension.js
@@ -1,20 +1,3 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
 
 import GObject from 'gi://GObject';
 import St from 'gi://St';
@@ -26,7 +9,6 @@ import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 // Configurable variables
-const CONFETTI_COUNT = 280;
 const GRAVITY = 0.25;
 const INITIAL_SPEED_Y = 15;
 const SPEED_X_RANGE = 4;
@@ -139,6 +121,15 @@ function animateConfetti() {
 }
 
 function startConfetti() {
+    if (animationLoop !== null) {
+        GLib.Source.remove(animationLoop); // Cancel previous loop
+        animationLoop = null;
+    }
+
     createConfetti();
-    GLib.timeout_add(GLib.PRIORITY_DEFAULT, FRAME_INTERVAL, animateConfetti);
+    animationLoop = GLib.timeout_add(
+        GLib.PRIORITY_DEFAULT,
+        FRAME_INTERVAL,
+        animateConfetti,
+    );
 }
